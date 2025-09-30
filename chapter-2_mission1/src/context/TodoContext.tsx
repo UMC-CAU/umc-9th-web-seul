853e772e0167bedce114f18d40f2ed3ad6253cf8
yeahsel <yeahsel0608@gmail.com>
2025-09-30 14:14:32 +0900

    week2 mission1

---
+++ chapter-2_mission1/src/context/TodoContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState, type PropsWithChildren } from 'react';
+import type {TTodo} from '../types/todo';
+
+interface ITodoContext {
+    todos: TTodo[];
+    doneTodos: TTodo[];
+    addTodo: (text: string) => void;
+    deleteTodo: (todo: TTodo) => void;
+    completeTodo: (todo: TTodo) => void;
+}
+
+export const TodoContext = createContext<ITodoContext | undefined> (undefined);
+
+export const TodoProvider = ({ children }:PropsWithChildren) => {
+
+    const [todos, setTodos] = useState<TTodo[]>([]);
+    const [doneTodos, setDoneTodos] = useState<TTodo[]>([]);
+
+    const addTodo = (text: string) => {
+        const newTodo: TTodo = { id:Date.now(), text };
+            setTodos((prevTodos) : TTodo[] => [...prevTodos, newTodo]);
+    }
+
+    const completeTodo = (todo: TTodo) => {
+        setTodos((prevTodos) : TTodo[] => prevTodos.filter((t) : boolean => t.id!== todo.id));
+        setDoneTodos((prevDoneTodos) : TTodo[] => [...prevDoneTodos, todo]);
+    };
+
+    const deleteTodo = (todo: TTodo) => {
+        setDoneTodos((prevDoneTodo) => 
+        prevDoneTodo.filter((t) => t.id !== todo.id));
+    };
+
+    return (
+        <TodoContext.Provider 
+        value={{ todos, doneTodos, addTodo, completeTodo, deleteTodo}}>
+            {children}
+        </TodoContext.Provider>
+    );
+};
+
+export const useTodo = () => {
+    const context = useContext(TodoContext)
+    if(!context) {
+        throw new Error(
+            'useTodo를 사용하기 위해서는, 무조건 TodoProvider로 감싸야 합니다.'
+        );
+    }
+    return context;
+}
