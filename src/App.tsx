75f512b5efa2c6da003c8df437b69f5b4a15c318
yeahsel <yeahsel0608@gmail.com>
2025-11-03 09:20:15 +0900

    mission 4-2, 4-3

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css'
+import HomePage from './pages/HomePage';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import HomeLayout from './layouts/HomeLayout';
+import SignupPage from './pages/SignupPage';
+import MyPage from './pages/MyPage';
+import { AuthProvider } from './context/AuthContext';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {index: true, element: <HomePage /> },
+      {path: 'login', element: <LoginPage />},
+      {path: 'signup', element: <SignupPage />},
+      {path: 'my', element: <MyPage />},
+    ]
+  },
+]);
+
+function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { getMyInfo } from "../apis/auth";
+import { useState } from "react";
+import type { ResponseMyInfoDto } from "../types/auth";
+
+const MyPage = () => {
+    const [data, setData] = useState<ResponseMyInfoDto>([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await getMyInfo();
+            console.log(response);
+
+            setData(response);
+        };
+
+        getData();
+    }, []);
+
+    console.log(data.data);
+    return <div>{data.data.name}</div>
+};
+
+export default MyPage;
