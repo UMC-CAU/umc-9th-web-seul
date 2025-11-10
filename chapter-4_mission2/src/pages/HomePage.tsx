4b82d41c5079e0e3de28311a149e63355cd401ee
yeahsel <yeahsel0608@gmail.com>
2025-11-11 04:27:47 +0900

    chapter6 mission1

--- chapter-4_mission2/src/pages/HomePage.tsx
+++ chapter-4_mission2/src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
-
+import useGetLpList from "../hooks/queries/useGetLpList.ts";
 
 const HomePage = () => {
+    const {data, isPending, error} = useGetLpList({});
+
     return (
-        <div>HomePage</div>
+        <div>
+            {data?.data.data.map((lp) => <h1>{lp.title}</h1>)};
+        </div>
     );
 };
 
